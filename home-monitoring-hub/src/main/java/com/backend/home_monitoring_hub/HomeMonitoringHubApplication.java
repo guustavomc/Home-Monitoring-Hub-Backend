d0b289fb922bc30af3acd96b90d2b535cb82c15e
guustavomc <gustavomccb@gmail.com>
2026-06-09 20:59:38 -0300

    Initialize project structure with Maven, add application properties, and create main application class

---
+++ home-monitoring-hub/src/main/java/com/backend/home_monitoring_hub/HomeMonitoringHubApplication.java
@@ -0,0 +1,13 @@
+package com.backend.home_monitoring_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMonitoringHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeMonitoringHubApplication.class, args);
+	}
+
+}
